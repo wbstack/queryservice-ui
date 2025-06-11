89a344e9ddc3d0b12c06238d274db5dea86c9da0
Jakob Warkotsch <jakob.warkotsch@wikimedia.de>
2025-06-11 17:41:58 +0200

    Add SparqlClassifier.isSimpleQuery()
    Bug: T396596
    Change-Id: I81aac9c3c6af6ddeddd84b0ee274478e2def437a

--- wikibase/tests/queryService/services/SparqlClassifier.test.js
+++ wikibase/tests/queryService/services/SparqlClassifier.test.js
@@ -8,13 +8,18 @@
 	fetch( 'queryService/services/SparqlClassifier.test.data.json' )
 		.then( response => response.json() )
 		.then( testData => {
-			QUnit.test.each( 'When instantiated with classified queries then', testData, function ( assert, fixture ) {
+			QUnit.test.each( 'When called with classified queries then', testData, function ( assert, fixture ) {
 				var q = new PACKAGE.SparqlClassifier();
-				var classifications = q.classify( fixture.query );
-				assert.deepEqual(
-					classifications,
-					fixture.expected,
-					'classifications should match expected values:\n' + fixture.query
+
+				assert.strictEqual(
+					q.classify( fixture.query ),
+					fixture.expectedClass,
+					'classification should match expected value:\n' + fixture.query
+				);
+				assert.strictEqual(
+					q.isSimpleQuery( fixture.query ),
+					fixture.isSimpleQuery,
+					'classifier should detect simple/complex query:\n' + fixture.query
 				);
 			} );
 		} )
