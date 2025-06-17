9c5e5522151feee08a2c0303a7299870366766c4
Jakob Warkotsch <jakob.warkotsch@wikimedia.de>
2025-06-17 13:49:39 +0200

    Allow modern js for browser tests
    Being able to use async/await makes these tests a lot easier to read and
    write.
    
    Change-Id: Icaf91d34756dc8504f6b3a7c4725ad8d1d7e233a

--- tests/specs/embed_spec.js
+++ tests/specs/embed_spec.js
@@ -1,31 +1,24 @@
-/* jshint strict: false */
-/* globals require, describe, it, browser, $ */
-var assert = require( 'assert' );
+'use strict';
+const assert = require( 'assert' );
 
-describe( 'embed.html', function () {
-	it( 'loads results for query', function () {
-		var query =
-			'SELECT ?item ?itemLabel ?other WHERE { '
-			+ ' VALUES (?item ?itemLabel ?other) { '
-			+ '     (wd:Q42 "Douglas Adams"@en "1952-03-11T00:00:00Z"^^xsd:dateTime) '
-			+ '     (wd:Q80 "Tim Berners-Lee"@de <http://commons.wikimedia.org/wiki/Special:FilePath/Sir%20Tim%20Berners-Lee%20%28cropped%29.jpg>)'
-			+ ' } }';
+describe( 'embed.html', () => {
+	it( 'loads results for query', async () => {
+		const query = `
+			SELECT ?item ?itemLabel ?other WHERE {
+			 VALUES (?item ?itemLabel ?other) {
+			     (wd:Q42 "Douglas Adams"@en "1952-03-11T00:00:00Z"^^xsd:dateTime)
+			     (wd:Q80 "Tim Berners-Lee"@de <http://commons.wikimedia.org/wiki/Special:FilePath/Sir%20Tim%20Berners-Lee%20%28cropped%29.jpg>)
+			} }`;
 
-		var url = browser.options.baseUrl + '/embed.html#' + encodeURI( query );
+		const url = browser.options.baseUrl + '/embed.html#' + encodeURI( query );
 		browser.url( url );
 
-		return $( '#query-result' ).then( function ( element ) {
-			element.waitForDisplayed();
+		const results = $( '#query-result' );
+		await results.waitForDisplayed();
 
-			return $( '#query-result tr' );
-		} ).then( function ( resultRows ) {
-			resultRows.waitForDisplayed();
-
-			return resultRows.getText();
-		} ).then( function ( resultHeaders ) {
-			assert( resultHeaders.includes( 'item' ) );
-			assert( resultHeaders.includes( 'other' ) );
-			assert( resultHeaders.includes( 'itemLabel' ) );
-		} );
+		const resultHeaders = await results.$( 'tr' ).getText();
+		assert( resultHeaders.includes( 'item' ) );
+		assert( resultHeaders.includes( 'other' ) );
+		assert( resultHeaders.includes( 'itemLabel' ) );
 	} );
 } );
